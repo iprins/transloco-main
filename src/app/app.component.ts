a74a00d084b4e69cbe91bdb1dabf8894f6e3ccc2
Iskandar Prins <iprins@almostlevel5.com>
2021-11-04 15:31:11 +0100

    Initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
+import { MyLibService } from 'my-workspace/dist/my-lib';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-tour-of-heroes';
+
+  constructor(private readonly translocoService: TranslocoService, private libService: MyLibService){}
+
+  setLang(lang: 'en' | 'es') {
+    this.translocoService.setActiveLang(lang);
+    // this.libService.setLang(lang);
+  }
+
+}
---
+++ transloco.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  rootTranslationsPath: 'src/assets/i18n/',
+  langs: ['en', 'es'],
+  keysManager: {},
+  scopedLibs: ['my-workspace/dist/my-lib']
+};
